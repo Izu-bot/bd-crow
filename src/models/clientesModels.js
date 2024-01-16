c1e15d44f69b330a3ae7700c046de081d3197c07
ZxwQ <kauanmartins977@gmail.com>
2024-01-16 10:45:08 -0300

    att

--- src/models/clientesModels.js
+++ src/models/clientesModels.js
@@ -18,17 +18,15 @@ const addCliente = async (cliente) => {
     const client = await connection()
 
     try {
-        const { name, surname, address, cpf, date} = cliente
-        
+        const { name, surname, address, cpf, date } = cliente
+        const sql = 'insert into tb_cliente (nm_cliente, ds_sobrenome, ds_endereco, ds_cpf, dt_nascimento) values ($1, $2, $3, $4, $5)'
+
         const convertDate = new Date(date)
-        const formatDate = convertDate.toISOString()
-
-
-        const sql = 'insert into tb_cliente (nm_cliente, ds_sobrenome, ds_endereco, ds_cpf, dt_nascimento) values ($1, $2, $3, $4, $5);'
-
+        const formatDate = convertDate.toDateString()
+        
         const createdClient = await client.query(sql, [name, surname, address, cpf, formatDate])
         return createdClient
-    } catch(err) {
+    } catch (err) {
         console.log(err)
     } finally {
         console.log('Fechando conexão...')
@@ -42,7 +40,7 @@ const deleteCliente = async (id) => {
     try {
         const removeClient = await client.query('delete from tb_cliente where id_cliente = $1;', [id])
         return removeClient
-    } catch(err) {
+    } catch (err) {
         console.log(err)
     } finally {
         console.log('Fechando conexão...')
@@ -54,13 +52,13 @@ const updateCliente = async (id, cliente) => {
     const client = await connection()
 
     try {
-        const {address} = cliente
-        
+        const { address } = cliente
+
         const updateCliente = await client.query('update tb_cliente set ds_endereco = $1 where id_cliente = $2', [address, id])
         return updateCliente
-    } catch(err) {
+    } catch (err) {
         console.log(err)
-    }finally {
+    } finally {
         console.log('Fechando conexão...')
         await client.end()
     }

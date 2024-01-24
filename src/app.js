7a46597dca61453fb3ae1d5fb34ed559334a1dde
ZxwQ <kauanmartins977@gmail.com>
2024-01-24 12:56:03 -0300

    v1

--- src/app.js
+++ src/app.js
@@ -1,20 +1,12 @@
 const express = require('express')
 const router = require('./router')
 const cors = require('cors')
-const session = require('express-session')
-require('dotenv').config();
-const secretKey = process.env.SECRET_KEY
 
 const app = express()
 
 app.use(express.json())
 app.use(cors())
-app.use(session({
-    secret: secretKey,
-    resave: false, 
-    saveUninitialized: true,
-    cookie: {secure: false}
-}))
+
 app.use(router)
 
 module.exports = app;
